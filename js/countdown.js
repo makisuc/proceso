52c214e96f97c112f54ef52f260c798d5cd4aa5e
makisuc <106550500+makisuc@users.noreply.github.com>
2022-07-09 02:02:00 +0200

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -1,7 +1,7 @@
 simplyCountdown('#cuenta', {
 	year: 2022, // required
 	month: 7, // required
-	day: 9, // required
+	day: 10, // required
 	hours: 7, // Default is 0 [0-23] integer
 	minutes: 31, // Default is 0 [0-59] integer
 	seconds: 0, // Default is 0 [0-59] integer
